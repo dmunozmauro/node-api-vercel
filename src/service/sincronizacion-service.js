ba63ee234a91b36ce34b9cc1447a2f76baf7d087
dmunoz <dmunoz@aligare.cl>
2023-06-24 19:57:59 -0400

    nueva data

--- src/service/sincronizacion-service.js
+++ src/service/sincronizacion-service.js
@@ -1,4 +1,5 @@
 const SHA2 = require("sha2");
+import { getObtenerData } from '../database/dao/sincronizacion-dao'
 
 
 export const CodehashClave = async (req, res) => {
@@ -7,7 +8,9 @@ export const CodehashClave = async (req, res) => {
         const clave = "Diego0831."
         const shaPass = Buffer.from(SHA2["SHA-224"](clave)).toString('base64');
 
-        res.status(200).send({ shaPass });
+        const data = await getObtenerData();
+
+        res.status(200).send({ shaPass, data });
     } catch (e) {
         console.log(e);
         res.status(500).send({ message: 'La operación no se pudo concretar, favor intente más tarde.' });
